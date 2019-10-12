0837d53e426b296dfce6a02835ddf9b38ab35471
Angel Retana <ratanarivas@gmail.com>
2019-10-12 17:44:23 -0500

    add the form to add a new task

--- save_task.php
+++ save_task.php
@@ -1,7 +1,18 @@
 <?php
 
+include("db.php");
+
 if (isset($_POST['save_task'])) {
-    echo "saving";
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    
+    $query = "INSERT INTO task(title,description) VALUES ('$title', '$description')";
+    $result = mysqli_query($conn, $query);
+
+    if (!$result) {
+        die("something went wrong inserting '$title'");
+    }
+    echo 'saved';
 }
 
 ?>

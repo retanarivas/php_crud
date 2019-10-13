d20d64a4238f919e8b84d27dfe890a7838dd29a7
Angel Retana <ratanarivas@gmail.com>
2019-10-12 20:22:04 -0500

    add method to update a task

--- update_task.php
+++ update_task.php
@@ -11,17 +11,15 @@
             $row = mysqli_fetch_array($result);
             $title = $row['title'];
             $description = $row['description'];
-            
-
-            
         }
-       
+    }
 
+    if(isset($_POST['update_task'])) {
+        $id = $_GET['id'];
+        $title = $_POST['title'];
+        $description = $_POST['description'];
 
-
-
-
-       /* $query = "UPDATE task SET (title = $title ,description = $description) WHERE id = $id";
+        $query = "UPDATE task SET title = '$title' ,description = '$description' WHERE id = $id";
 
        $result = mysqli_query($conn, $query);
 
@@ -32,7 +30,32 @@
         $_SESSION['message'] = 'Taks updated succesfully';
         $_SESSION['message_type'] = 'success';
 
-        header("Location: index.php"); */
+        header("Location: index.php");
+        
     }
 
 ?>
+
+<?php include("includes/header.php"); ?>
+
+    <div class="container p-4">
+        <div class="row">
+            <div class="col-md-4 mx-auto">
+
+                <div class="card card-body">
+                    <form action="update_task.php?id=<?php echo $id?>" method="POST">
+                        <div class="form-group">
+                            <input type="text" name="title" value="<?php echo $title; ?>" class="form-control" placeholder="Update Title" autofocus>
+                        </div>
+                        <div class="form-group">
+                            <textarea name="description" rows="2" placeholder="Update description" class="form-control"><?php echo $description; ?></textarea>
+                        </div>
+                        <input type="submit" value="Update" class="btn btn-success btn-block" name="update_task">
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+<?php include("includes/footer.php"); ?>
